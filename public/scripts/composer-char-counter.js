f579eced9920332141a08ab88676f5cc3ef70d6f
Jonathan Lao <jon.lao@gmail.com>
2023-12-06 12:46:50 -0600

    add tweet character counter and change avatar image + clean-up

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  const $tweetText = $("#tweet-text");
+  const $count = $("#counter");
+
+// show characters countdown
+  $tweetText.on("input", () => {
+    const val = $tweetText.val();
+    const numOfChar = val.length;
+    const charLeft = 140 - numOfChar;
+    $count.text(charLeft);
+
+    const tooMuch = charLeft < 0;
+    $count.toggleClass("red-text", tooMuch);
+  });
+
+
+});
