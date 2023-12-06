d722cc36768edde8505ff5d4c0d3c87665d482a7
Jonathan Lao <jon.lao@gmail.com>
2023-12-06 17:02:14 -0600

    Implement renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-$(document).ready(() => {
+const createTweetElement = (tweet) => {
+  const { user, content, created_at } = tweet;
+  const { name, avatars, handle } = user;
+  const { text } = content;
+  const timeAgo = timeago.format(created_at);
   
-  const createTweetElement = (tweetObj) => {
-    const { user, content, created_at } = tweetObj;
-    const { name, avatars, handle } = user;
-    const { text } = content;
-    const timeAgo = timeago.format(created_at);
-
-    const $tweet = $(`
+  const $tweet = $(`
     <article class="tweet">
     <header>
-    <div class="first-name">
+      <div class="first-name">
         <img src=${avatars}>
         ${name}
       </div>
@@ -50,15 +34,47 @@ $(document).ready(() => {
         <i class="fas fa-heart fa-sm"></i>
       </div>
     </footer>
-  </article>
-  `);
-    return $tweet;
+    </article>
+    `);
+      return $tweet;
+    };
+      
+  //const $tweet = createTweetElement(tweetData);
+
+  const renderTweets = function(tweets) {
+    for (const key in tweets) {
+      const info = tweets[key];
+      const $tweet = createTweetElement(info);
+      $('#tweets-container').append($tweet);
+    }
   };
 
-  const $tweet = createTweetElement(tweetData);
+//  / Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+$(document).ready(() => {
+  renderTweets(data);
 });
